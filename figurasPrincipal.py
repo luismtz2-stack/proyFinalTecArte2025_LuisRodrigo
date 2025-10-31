423a65aa69ae3ec3d4541eb97c073bc5351db112
NAME <EMAIL>
2025-10-31 15:44:57 -0600

    Primer commit: cálculo de áreas y funciones

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,10 +1,26 @@
 import pandas as pd
+from funciones import triangulo, circulo, rectangulo
 
-dataFile = pd.read_csv("figuras.csv")
+dataFile = pd.read_csv("figuras.csv", delim_whitespace=True)
+
+print("Procesando figuras ...\n")
 
 areas = []
-perimetros = []
-
 
 for index, row in dataFile.iterrows():
-        print(f"Fila {index}: Figura={row['FIGURA']}, Medida1={row['Medida1']}, Medida2={row['MEDIDA2']}")
+    FIGURA = row['FIGURA']
+    MEDIDA1 = row['MEDIDA1']
+    MEDIDA2 = row['MEDIDA2']
+
+    if FIGURA == 't':  # triángulo
+        area = triangulo(MEDIDA1, MEDIDA2)
+    elif FIGURA == 'r':  # rectángulo
+        area = rectangulo(MEDIDA1, MEDIDA2)
+    elif FIGURA == 'c':  # círculo
+        area = circulo(MEDIDA1)
+    else:
+        area = None
+
+    areas.append(area)
+    print(f"Fila {index}: FIGURA={FIGURA}, MEDIDA1={MEDIDA1}, MEDIDA2={MEDIDA2}, AREA={area}")
+
