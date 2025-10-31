c8fd7be345221efc95a105ef76903787b856bc11
NAME <EMAIL>
2025-10-31 15:54:26 -0600

    funciones codigo

---
+++ funciones.py
@@ -0,0 +1,11 @@
+import math
+
+def triangulo(MEDIDA1, MEDIDA2):
+        t = (MEDIDA1 * MEDIDA2) / 2
+        return t
+def circulo(MEDIDA1):
+        c = math.pi * (MEDIDA1 ** 2)
+        return c
+def rectangulo(MEDIDA1, MEDIDA2):
+        r = MEDIDA1 * MEDIDA2
+        return r
